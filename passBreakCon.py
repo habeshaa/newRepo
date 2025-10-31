d4e10d855c84e18d6d86b29ed1e33881d39e8286
thomas addisu <addisuthomas8@gmail.com>
2025-10-31 22:16:08 +0300

    pass,break and continue in python

---
+++ passBreakCon.py
@@ -0,0 +1,23 @@
+# pass  Does nothing — it’s just a placeholder.
+print("pass in py")
+for i in range(5):
+    if i==3:
+        pass
+    print(i)
+
+# break   Stops the entire loop immediately.   Used when you want to exit a loop early
+print("break in py")
+for i in range(5):
+    if i==3:
+        break
+    print(i)
+
+# continue  Skips the current iteration and moves to the next one.
+
+print("continue in py")
+for i in range(5):
+    if i==3:
+        continue
+    print(i)
+
+print("subscribe")
